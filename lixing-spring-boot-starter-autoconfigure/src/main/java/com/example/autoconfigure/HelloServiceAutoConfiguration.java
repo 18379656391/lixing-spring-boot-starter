d8ae6879e9eb26ef6a34e0b7669e036534dc7442
lixing41189 <lixing41189@hundsun.com>
2024-11-20 18:38:40 +0800

    first

---
+++ lixing-spring-boot-starter-autoconfigure/src/main/java/com/example/autoconfigure/HelloServiceAutoConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.autoconfigure;
+
+import com.example.service.HelloService;
+import com.example.properties.HelloProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+@Configuration
+@ConditionalOnWebApplication
+@EnableConfigurationProperties(HelloProperties.class)
+public class HelloServiceAutoConfiguration {
+    @Autowired
+    private HelloProperties helloProperties;
+
+    @Bean
+    public HelloService helloService() {
+        HelloService helloService = new HelloService();
+        helloService.setHelloProperties(helloProperties);
+        return helloService;
+    }
+
+
+}
